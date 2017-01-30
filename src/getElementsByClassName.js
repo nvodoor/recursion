356344c32698fbbe191468133f62940d5ca52a4c
nvodoor <nvodoor@gmail.com>
2017-01-29 16:22:28 -0800

    Finished. Optimal work. Great job Nate.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className){
+  var element = [];
+
+
+
+
+  function child(parentNode){
+    var parent = parentNode.childNodes;
+    for (var i = 0; i < parent.length; i++){
+        var classes = parent[i].classList;
+        if (classes !== undefined && classes.contains(className)) {
+            element.push(parent[i]);
+        }
+        if (parent[i].childNodes[0]){
+            child(parent[i]);
+        }
+    }
+  }
+  child(document);
+  console.log(element)
+  return element;
 };
